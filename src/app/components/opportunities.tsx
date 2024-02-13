cf94521fb7a18e29c3beb9ae8ce604399d94fc2e
Alan Johnson <alan@breakrs.com>
2024-02-12 22:39:45 -0500

    add scroll shadow to opportunities

--- src/app/components/opportunities.tsx
+++ src/app/components/opportunities.tsx
@@ -1,3 +1,4 @@
+import { ScrollShadow } from "@nextui-org/react";
 import { Opportunity } from "../lib/models";
 import styles from "../page.module.css";
 
@@ -18,46 +19,58 @@ export const Opportunities: React.FunctionComponent<{
   console.dir({ opportunitiesByStatus }, { depth: null });
   return (
     <section className={styles.opportunitiesSection}>
-      <h2 className={styles.opportunitiesSection__header}>Opportunities</h2>
-      <ul className={styles.opporunityStatuses}>
-        {ORDERED_STATUSES.map((status) => (
-          <li className={styles.opportunityStatusColumn} key={status}>
-            <h3 className={styles.opportunityStatusColumn__header}>{status}</h3>
-            <ul className={styles.opportunityStatusColumn__stack}>
-              {opportunitiesByStatus[status]?.map((opportunity) => {
-                const logoUrl =
-                  opportunity.type === "Connection"
-                    ? "/Generic_friend_style_2.png"
-                    : opportunity.logoDomain
-                    ? `https://logo.clearbit.com/${opportunity.logoDomain}`
-                    : "/Generic_company_style_2.png";
-                const logoAlt = opportunity.logoDomain
-                  ? `${opportunity.name} logo`
-                  : "Generic logo";
-                return (
-                  <li className={styles.opportunityCard} key={opportunity.id}>
-                    <div className={styles.opportunityCard__topRow}>
-                      <img
-                        src={logoUrl}
-                        alt={logoAlt}
-                        className={styles.opportunityCard__logo}
-                      />
-                      <div className={styles.opportunityCard__description}>
-                        <div className={styles.opportunityCard__name}>
-                          {opportunity.name}
+      <ScrollShadow orientation="vertical">
+        <ScrollShadow orientation="horizontal">
+          <h2 className={styles.opportunitiesSection__header}>Opportunities</h2>
+          <ul className={styles.opporunityStatuses}>
+            {ORDERED_STATUSES.map((status) => (
+              <li className={styles.opportunityStatusColumn} key={status}>
+                <h3 className="flex justify-between mb-2 p-1">
+                  <span>{status}</span>
+                  <span className="text-xs">
+                    {opportunitiesByStatus[status]?.length ?? 0}
+                  </span>
+                </h3>
+                <ul className={styles.opportunityStatusColumn__stack}>
+                  {opportunitiesByStatus[status]?.map((opportunity) => {
+                    const logoUrl =
+                      opportunity.type === "Connection"
+                        ? "/Generic_friend_style_2.png"
+                        : opportunity.logoDomain
+                        ? `https://logo.clearbit.com/${opportunity.logoDomain}`
+                        : "/Generic_company_style_2.png";
+                    const logoAlt = opportunity.logoDomain
+                      ? `${opportunity.name} logo`
+                      : "Generic logo";
+                    return (
+                      <li
+                        className={styles.opportunityCard}
+                        key={opportunity.id}
+                      >
+                        <div className={styles.opportunityCard__topRow}>
+                          <img
+                            src={logoUrl}
+                            alt={logoAlt}
+                            className={styles.opportunityCard__logo}
+                          />
+                          <div className={styles.opportunityCard__description}>
+                            <div className={styles.opportunityCard__name}>
+                              {opportunity.name}
+                            </div>
+                            <div className={styles.opportunityCard__title}>
+                              {opportunity.title}
+                            </div>
+                          </div>
                         </div>
-                        <div className={styles.opportunityCard__title}>
-                          {opportunity.title}
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </li>
-        ))}
-      </ul>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </ScrollShadow>
+      </ScrollShadow>
     </section>
   );
 };
